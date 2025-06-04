bb89f03fe5fb167f061630afbe1b0a72c6267910
profjuanpablo <prof.juanpablo@gmail.com>
2025-06-04 09:16:13 -0300

    Receveing data in the view

--- ECommerce/Controllers/CategoryController.cs
+++ ECommerce/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerce.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }

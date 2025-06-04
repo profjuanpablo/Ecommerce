788b9684c42b8700faf17e1eb40b1ee600f66337
profjuanpablo <prof.juanpablo@gmail.com>
2025-06-04 08:56:55 -0300

    Seed Category

--- ECommerce/Data/ApplicationDbContext.cs
+++ ECommerce/Data/ApplicationDbContext.cs
@@ -12,5 +12,14 @@ namespace ECommerce.Data
         }
 
         public DbSet<Category>  Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1},
+                new Category { Id = 2, Name = "Adventure", DisplayOrder = 2 }, 
+                new Category { Id = 3, Name = "Role Playing", DisplayOrder = 3 }
+                );
+        }
     }
 }

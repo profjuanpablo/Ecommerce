99863292802e6bc48949da76369d574c903c1761
profjuanpablo <prof.juanpablo@gmail.com>
2025-06-07 20:30:51 -0300

    Input tag Helpers

--- ECommerce/Models/Category.cs
+++ ECommerce/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ECommerce.Models
 {
@@ -8,6 +9,9 @@ namespace ECommerce.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
